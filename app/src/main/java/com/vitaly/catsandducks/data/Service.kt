94a406e9aea185677176994e90790b5756ab1f62
Vitaly <kuraev_vf@mail.ru>
2021-09-21 20:40:53 +0500

    migrate retrofit executor -> rx

--- app/src/main/java/com/vitaly/catsandducks/data/Service.kt
+++ app/src/main/java/com/vitaly/catsandducks/data/Service.kt
@@ -1,13 +1,14 @@
 package com.vitaly.catsandducks.data
 
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import java.io.Serializable
 
 interface Service {
     @GET("random")
-    fun getDuck(): Call<DuckResponse>
+    fun getDuck(): Observable<DuckResponse>
 
     @GET("rest")
-    fun getCat(): Call<CatResponse>
+    fun getCat(): Observable<CatResponse>
 }
--- app/src/main/java/com/vitaly/catsandducks/presentation/MainActivity.kt
+++ app/src/main/java/com/vitaly/catsandducks/presentation/MainActivity.kt
@@ -1,87 +1,66 @@
 package com.vitaly.catsandducks.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.vitaly.catsandducks.R
-import com.vitaly.catsandducks.data.CatResponse
 import com.vitaly.catsandducks.data.Constants
-import com.vitaly.catsandducks.data.DuckResponse
 import com.vitaly.catsandducks.data.Service
 import com.vitaly.catsandducks.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private var doubleClickLastTime = 0L
+    lateinit var compositeDisposable: CompositeDisposable
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnShowDuck.setOnClickListener {
-            val listCall: Call<DuckResponse> = createService(Constants.DUCK_BASE_URL).getDuck()
-            listCall.enqueue(object : Callback<DuckResponse> {
-                override fun onResponse(
-                    call: Call<DuckResponse>,
-                    response: Response<DuckResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        val duckResponse: DuckResponse? = response.body()
-                        if (duckResponse != null) {
-                            Glide
-                                .with(binding.ivPic)
-                                .load(duckResponse.url)
-                                .into(binding.ivPic)
-                        }
-                    }
-                }
-                override fun onFailure(call: Call<DuckResponse>, t: Throwable) {
-                    Log.e("Error!", t.message.toString())
-                }
-            })
+            compositeDisposable = CompositeDisposable()
+            compositeDisposable.add(
+                createService(Constants.DUCK_BASE_URL).getDuck()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe { response -> onResponse(response.url) })
         }
 
         binding.btnShowCat.setOnClickListener {
-            val listCall: Call<CatResponse> = createService(Constants.CAT_BASE_URL).getCat()
-            listCall.enqueue(object : Callback<CatResponse> {
-                override fun onResponse(
-                    call: Call<CatResponse>,
-                    response: Response<CatResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        val catResponse: CatResponse? = response.body()
-                        if (catResponse != null) {
-                            Glide
-                                .with(binding.ivPic)
-                                .load(catResponse.url)
-                                .into(binding.ivPic)
-                        }
-                    }
-                }
-                override fun onFailure(call: Call<CatResponse>, t: Throwable) {
-                    Log.e("Error!", t.message.toString())
-                }
-            })
+            compositeDisposable = CompositeDisposable()
+            compositeDisposable.add(
+                createService(Constants.CAT_BASE_URL).getCat()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe { response -> onResponse(response.url) })
         }
         binding.ivPic.setOnClickListener {
-            if(System.currentTimeMillis() - doubleClickLastTime < 300){
+            if (System.currentTimeMillis() - doubleClickLastTime < 300) {
                 doubleClickLastTime = 0
                 Toast.makeText(this, getString(R.string.pic_saved), Toast.LENGTH_SHORT).show()
-            }else doubleClickLastTime = System.currentTimeMillis()
+            } else doubleClickLastTime = System.currentTimeMillis()
         }
     }
 
     private fun createService(baseUrl: String): Service {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         return retrofit.create(Service::class.java)
+    }
+
+    private fun onResponse(url: String) {
+        Glide
+            .with(binding.ivPic)
+            .load(url)
+            .into(binding.ivPic)
     }
 }
